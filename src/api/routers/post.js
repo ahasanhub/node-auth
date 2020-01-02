7dc3b2065ab220368db6e54fa0c282f31d1af974
Zayn An Nisa <shobuz_cse03@yahoo.com>
2020-01-02 22:53:28 +0600

    update post route

--- src/api/routers/post.js
+++ src/api/routers/post.js
@@ -31,6 +31,38 @@ router.route('/')
                 error: error.message
             });
         }
+    });
+
+//Read single post
+router.route('/:id')
+    .get(auth, async (req, res) => {
+        const _id = req.params.id;
+        if (!ObjectID.isValid(_id)) {
+            return res.status(400).send({
+                error: 'Bad request'
+            })
+        }
+        try {
+            const post = await Post.findOne({
+                _id,
+                author: req.user._id
+            });
+            if (!post) {
+                return res.status(200).send(post);
+            }
+        } catch (error) {
+            res.status(500).send({
+                error: 'Internal server error'
+            });
+        }
     })
+    .put(auth, async (req, res) => {
+
+    })
+    .delete(auth, async (req, res) => {
+
+    })
+
+
 
 module.exports = router;
