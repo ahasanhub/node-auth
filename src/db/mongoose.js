ba224613a69a2b6b5fa31c08a0ea63b747c6c2cc
Zayn An Nisa <shobuz_cse03@yahoo.com>
2020-01-04 20:39:10 +0600

    update minor

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://nodeauth:node123456@ds039950.mlab.com:39950/node-auth', {
+const uri = 'mongodb://nodeauth:node123456@ds039950.mlab.com:39950/node-auth';
+const options = {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-}).then(() => console.log('connected to database')).catch(() => console.log('failed connected to database'));
+}
+mongoose.connect(uri, options)
+    .then(() => console.log('connected to database'))
+    .catch(() => console.log('failed connected to database'));
 
-module.exports = mongoose
+module.exports = mongoose;
