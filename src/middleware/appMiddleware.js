f1488319be514ebc320112d02c9103b7ef86b642
Ahasan Habib <shobuz.cse03@gmail.com>
2020-01-05 11:31:41 +0600

    fixed cors permissions

--- src/middleware/appMiddleware.js
+++ src/middleware/appMiddleware.js
@@ -1,8 +1,11 @@
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 //setup global middleware here
 
 module.exports = function (app) {
+    app.use(cors());
     app.use(morgan('dev'));
     app.use(bodyParser.urlencoded({
         extended: false
