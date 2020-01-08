997f5f367c05566219d7f0848623ef82a5e8b7e7
Ahasan Habib <shobuz.cse03@gmail.com>
2020-01-08 18:09:20 +0600

    minor fix

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 //api router will mount other routers
 //for all our resources
-router.use('/users', require('./routers/user'));
-router.use('/posts', require('./routers/post'));
+router.use('/users', require('./routes/user'));
+router.use('/posts', require('./routes/post'));
 
 module.exports = router;
