3c30ab7b718ce9f1edd861fb33db7856704a9bed
Zayn An Nisa <shobuz_cse03@yahoo.com>
2020-01-08 21:53:40 +0600

    update minor

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const api = require('./api/api');
+const apiRoute = require('./api/api');
 //connect mongodb
 require('./db/mongoose')
 //set middleware
 require('./middleware/appMiddleware')(app);
 //set router
-app.use('/api', api);
+app.use('/api', apiRoute);
 
 module.exports = app;
